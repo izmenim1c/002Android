9d199a32e80be46f4484656f887a0a80fc71b873
SamSebeSam <samsamoiloff@mail.ru>
2022-01-10 17:35:05 +0300

    Лишнее удалил, почему-то на 31 билд не найден пишет, поставил 30.0.0
    Начало третьего урока сделал, одну кнопку, без кода обработки нажатия

--- app/src/main/java/com/example/a002android003/Lesson003.java
+++ app/src/main/java/com/example/a002android003/Lesson003.java
@@ -3,12 +3,18 @@ package com.example.a002android003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Lesson003 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main003);
+ //       counters = new Counters();
+
+    }
+
+    public void button1_onClick(View view) {
     }
 }
