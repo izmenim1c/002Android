d844ce82888aa3979aac99e90b8caeb39dbf6247
SamSebeSam <samsamoiloff@mail.ru>
2022-01-11 20:31:12 +0300

    Первая кнопка со счетчиком нажатий онклик, что не надо использовать

--- app/src/main/java/com/example/a002android003/Lesson003.java
+++ app/src/main/java/com/example/a002android003/Lesson003.java
@@ -4,17 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class Lesson003 extends AppCompatActivity {
+
+    private TextView textCounter1;  // пользовательский элемент 1-го счетчика
+    private int counter1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main003);
+        initView();
  //       counters = new Counters();
 
     }
 
-    public void button1_onClick(View view) {
+    private void initView() {
+        // Получить пользовательские элементы по идентификатору
+        textCounter1 = findViewById(R.id.textView1);
+        //textCounter2 = findViewById(R.id.textView2);
+        //textCounter3 = findViewById(R.id.textView3);
+        //textCounter4 = findViewById(R.id.textView4);
+
+        //initButton2ClickListener();
+        //initButton3ClickListener();
+        //initButton4ClickListener();
     }
+    // Обработка кнопки через атрибут onClick в макете
+    public void button1_onClick(View view) {
+       // counters.incrementCounter2();
+        //setTextCounter(textCounter1, counters.getCounter2());
+        counter1++;
+        setTextCounter(textCounter1,counter1 );
+    }
+    // Отображение данных на экране
+    private void setTextCounters(){
+        //setTextCounter(textCounter1, counters.getCounter1());
+        //setTextCounter(textCounter2, counters.getCounter2());
+        //setTextCounter(textCounter3, counters.getCounter3());
+        //setTextCounter(textCounter4, counters.getCounter4());
+    }
+    // Установить текст на TextView
+    private void setTextCounter(TextView textCounter, int counter){
+        textCounter.setText(String.format(Locale.getDefault(), "%d", counter));
+    }
+
+
+
+
 }
